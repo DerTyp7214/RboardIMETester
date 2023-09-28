3cae8e7b67ef5dfe8232065f03f57da2e3d6a7f9
PahaAkos <paha.akos@gmail.com>
2023-09-28 19:33:52 +0200

    Add Android 6 Support, Switch to new library catalog declaration, Update Libs
    New dependencies changes:
    • Update com.android.tools.build:gradle to 8.3.0-alpha05
    • Update com.google.devtools.ksp to 1.9.10-1.0.13
    • Update org.jetbrains.kotlin:kotlin-gradle-plugin to 1.9.10
    • Update androidx.core:core to 1.12.0
    • Update androidx.core:core-ktx to 1.12.0
    • Update androidx.browser:browser to 1.6.0
    • Update androidx.appcompat:appcompat to 1.7.0-alpha03
    • Update com.google.android.material:material to 1.9.0
    • Update org.jetbrains.kotlin:kotlin-reflect to 1.9.10
    • Update org.jetbrains.kotlin:kotlin-stdlib to 1.9.10
    • Update androidx.lifecycle:lifecycle-viewmodel-ktx to 2.6.2
    • Update androidx.test.espresso:espresso-core to 3.5.1
    • Update androidx.test.ext:junit to 1.1.5
    • Added com.android.tools:desugar_jdk_libs 2.0.3
    
    Others Changes:
    • Update compileSdk to 34
    • Update targetSdk to 34
    • Update buildToolsVersion to 34.0.0
    • Update actions/checkout to v4
    • Update actions/setup-java to v3
    • Update actions/upload-artifact to v3
    • Update r0adkll/upload-google-play to v1.1.2
    • Update gradle-wrapper to 8.3
    • Update versionCode to 110000
    • Update versionName to 1.1.0
    • Update Java to 20

---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+import org.jetbrains.kotlin.config.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+
+android {
+    compileSdk = 34
+    buildToolsVersion = "34.0.0"
+    buildFeatures.dataBinding = true
+
+    buildFeatures.viewBinding = true
+    buildFeatures.buildConfig = true
+
+    namespace = "de.dertyp7214.rboardimetester"
+
+    defaultConfig {
+        applicationId = "de.dertyp7214.rboardimetester"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 110
+        versionName = "1.1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        getByName("debug") {
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+        }
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
+    }
+
+    kotlinOptions {
+        jvmTarget = JvmTarget.JVM_20.description
+        freeCompilerArgs += listOf(
+            "-P",
+            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=true"
+        )
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = JvmTarget.JVM_20.description
+        }
+    }
+
+    packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+    implementation(project(":colorutilsc"))
+    implementation(project(":rboardcomponents"))
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.legacy.support.v4)
+    implementation(libs.browser)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+rootProject.name = "Rboard IME Tester"
+include(":app")
+include(":colorutilsc")
+include(":rboardcomponents")
